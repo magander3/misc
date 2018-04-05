6382a37dc73dd89f20458da60b3f6330677dfb2f
Magnus Andersson <magander3@users.noreply.github.com>
2018-04-05 14:02:13 +0200

    PowerShell script to delete files and or folders
    PowerShell script to delete files and or folders in specific Windows Server folders based on age.

---
+++ PowerShell_delete-files-or-folders.ps1
@@ -0,0 +1,30 @@
+# PowerShell Script to delete files and or folders in specific Windows Server folders.
+# Created by: Magnus Andersson 
+# Version 1.0
+#
+# ---------------------
+# User input section starts here
+#
+# Define scfript log file
+$logfile="D:\scripts\deletefiles_folders.log"
+#
+# Define direcroty where the vCenter Server and NSX Manager backups are located
+$dir1="D:\applixcation-1\logsfiles"
+$dir2="D:\applixcation-2\logsfiles"
+#
+# Define how old backups you want to remove by typing a number which corresponds to days
+$deletebackupsolderthan="7"
+#
+# User input section end here - Do not change anything below this line
+# --------------------------------------------------------------------
+#
+# Delete logfile 
+Remove-Item $logfile
+# Start delete files and or folders script
+$d1=date
+echo "----------------------------------------" >> $logfile
+echo "Start deleting old backups at:" $d1 >> $logfile
+Get-ChildItem -Path "$dir1" -Recurse | Where CreationTime -lt  (Get-Date).AddDays(-$deletebackupsolderthan) | Remove-Item -Force -Recurse *>&1 >> $logfile
+Get-ChildItem -Path "$dir2" -Recurse | Where CreationTime -lt  (Get-Date).AddDays(-$deletebackupsolderthan) | Remove-Item -Force -Recurse *>&1 >> $logfile
+$d2=date
+echo "Finished deleting old files and or folders at:" $d2 >> $logfile
