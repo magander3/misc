3b9d0c0718b8a38cd371a4b438e479dd18ef0b3b
Magnus Andersson <magander3@users.noreply.github.com>
2018-04-06 09:00:27 +0200

    Update PowerShell_delete-files-or-folders.ps1

--- PowerShell_delete-files-or-folders.ps1
+++ PowerShell_delete-files-or-folders.ps1
@@ -10,7 +10,6 @@ $logfile="D:\scripts\deletefiles_folders.log"
 #
 # Define direcroty where the vCenter Server and NSX Manager backups are located
 $dir1="D:\applixcation-1\logsfiles"
-$dir2="D:\applixcation-2\logsfiles"
 #
 # Define how old backups you want to remove by typing a number which corresponds to days
 $deletefilesolderthan="7"
@@ -25,6 +24,5 @@ $d1=date
 echo "----------------------------------------" >> $logfile
 echo "Start deleting old backups at:" $d1 >> $logfile
 Get-ChildItem -Path "$dir1" -Recurse | Where CreationTime -lt  (Get-Date).AddDays(-$deletebackupsolderthan) | Remove-Item -Force -Recurse *>&1 >> $logfile
-Get-ChildItem -Path "$dir2" -Recurse | Where CreationTime -lt  (Get-Date).AddDays(-$deletebackupsolderthan) | Remove-Item -Force -Recurse *>&1 >> $logfile
 $d2=date
 echo "Finished deleting old files and or folders at:" $d2 >> $logfile
